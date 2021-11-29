c1ee5baf826bf8a57191f871711fbe5fe6b804b5
extremq <stefansteff999@gmail.com>
2021-11-29 22:02:13 +0200

    Typo.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 new webpack.DefinePlugin({
   "process.env": {
-    APIKEY: JSON.stringify(process.env.FIREBASE_KEY.substring(1, process.env.FIREBASE_KEY.length - 1)),
+    FIREBASE_KEY: JSON.stringify(process.env.FIREBASE_KEY.substring(1, process.env.FIREBASE_KEY.length - 1)),
     AUTHDOMAIN: JSON.stringify(process.env.AUTHDOMAIN.substring(1, process.env.AUTHDOMAIN.length - 1)),
     PROJECTID: JSON.stringify(process.env.PROJECTID.substring(1, process.env.PROJECTID.length - 1)),
     STORAGEBUCKET: JSON.stringify(process.env.STORAGEBUCKET.substring(1, process.env.STORAGEBUCKET.length - 1)),
