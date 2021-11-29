858d93f49958dfcfc41aafba6b0d65306fbf8670
extremq <stefansteff999@gmail.com>
2021-11-29 21:57:15 +0200

    Webpack env.

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+new webpack.DefinePlugin({
+  "process.env": {
+    APIKEY: JSON.stringify(process.env.FIREBASE_KEY.substring(1, process.env.FIREBASE_KEY.length - 1)),
+    AUTHDOMAIN: JSON.stringify(process.env.AUTHDOMAIN.substring(1, process.env.AUTHDOMAIN.length - 1)),
+    PROJECTID: JSON.stringify(process.env.PROJECTID.substring(1, process.env.PROJECTID.length - 1)),
+    STORAGEBUCKET: JSON.stringify(process.env.STORAGEBUCKET.substring(1, process.env.STORAGEBUCKET.length - 1)),
+    MESSAGINGSENDERID: JSON.stringify(process.env.MESSAGINGSENDERID.substring(1, process.env.MESSAGINGSENDERID.length - 1)),
+    APPID: JSON.stringify(process.env.APPID.substring(1, process.env.APPID.length - 1))
+  },
+});
